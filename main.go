5ad71180cbf54810cd62fe89d06dd2b34971766c
aphilly <51158596+AP5113246@users.noreply.github.com>
2025-09-20 17:58:54 +1000

    New Line content excercises

--- main.go
+++ main.go
@@ -6,13 +6,12 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 const inputFilePath = "messages.txt"
 
 func main() {
-	// open file and defer close
-
 	f, err := os.Open(inputFilePath)
 	if err != nil {
 		log.Fatalf("Could not open file titled : %s . Error : %s", inputFilePath, err)
@@ -22,18 +21,30 @@ func main() {
 
 	fmt.Printf("Reading data from %s\n", inputFilePath)
 	fmt.Println("=====================================")
+	newLineContent := ""
 
 	for {
-		b := make([]byte,8,8)
-		n, err := f.Read(b)
+		buffer := make([]byte,8,8)
+		n, err := f.Read(buffer)
 		if err != nil {
+			if newLineContent != "" {
+				fmt.Printf("read: %s",newLineContent)
+				newLineContent = ""
+			}
 			if errors.Is(err, io.EOF){
 				break
 			}
 			fmt.Printf("error: %s\n",err.Error())
 			break
 		}
-		str := string(b[0:n])
-		fmt.Printf("read: %v\n",str)
+
+		str := string(buffer[0:n])
+		parts := strings.Split(str,"\n")
+
+		for i := 0; i < len(parts) - 1; i++ {
+			fmt.Printf("read: %s%s\n", newLineContent,parts[i])
+			newLineContent = ""
+		}
+		newLineContent += parts[len(parts) - 1]
 	}
 }
