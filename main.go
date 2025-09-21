83d9407aec22f3d7e3bcafd2bd46f440171219aa
aphilly <51158596+AP5113246@users.noreply.github.com>
2025-09-21 21:23:07 +1000

    finalised solution for reading file to channel

--- main.go
+++ main.go
@@ -12,9 +12,11 @@ import (
 const inputFilePath = "messages.txt"
 
 func getLinesChannel(f io.ReadCloser) <-chan string {
-	lines := make(chan string)
+	lines := make(chan string, 1)
 
 	go func(){
+		defer f.Close()
+		defer close(lines)
 		newLineContent := ""
 		for {
 			buffer := make([]byte, 8,8)
@@ -22,7 +24,6 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 			if err != nil {
 				if newLineContent != "" {
 					lines <- newLineContent
-					newLineContent = ""
 				}
 				if errors.Is(err, io.EOF) {
 					break
@@ -40,10 +41,7 @@ func getLinesChannel(f io.ReadCloser) <-chan string {
 			}
 			newLineContent += parts[len(parts) - 1]			
 		}
-		close(lines) // what does close () do?
 	}()
-
-	
 	return lines 
 }
 
@@ -53,10 +51,8 @@ func main() {
 		log.Fatalf("Could not open file titled : %s . Error : %s", inputFilePath, err)
 
 	}
-	defer f.Close()
 
 	fmt.Printf("Reading data from %s\n", inputFilePath)    
-	fmt.Println("=====================================")
 	
 	linesFromChannel := getLinesChannel(f)
 
